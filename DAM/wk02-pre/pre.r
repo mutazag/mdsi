8c7d6305e763392f96fdaa91a3c9793c839acd2e
Mutaz Abu Ghazaleh <mutaza@microsoft.com>
2018-03-23 01:50:28 +1100

    DAM wk02 pre - finish excercise

--- DAM/wk02-pre/pre.r
+++ DAM/wk02-pre/pre.r
@@ -12,3 +12,75 @@ max(diamonds$price )
 diamonds[order(diamonds$price, decreasing = TRUE)[1], ]
 # or 2
 diamonds[diamonds$price == max(diamonds$price), ]
+
+
+
+# data packages 
+head(AirPassengers)
+str(AirPassengers)
+
+# list all data packages
+data(package = .packages(all.available = TRUE))
+
+head(mtcars)
+str(mtcars)
+rownames(mtcars)
+colnames(mtcars)
+summary(mtcars)
+
+
+# data types
+data <- mtcars
+str(data)
+data$cyl <- as.factor(data$cyl)
+data$cyl
+
+data$vs <- as.factor(data$vs)
+data$am <- as.factor(data$am)
+data$gear <- as.factor(data$gear)
+data$carb <-as.factor(data$carb)
+str(data)
+
+# create a column for car names (based on row names) 
+data$carname <- rownames(data)
+data
+
+# subsetting 
+head(data[,1:3])
+head(data[, c("mpg", "cyl", "disp")])
+
+
+# visualisation 
+
+hist(data$qsec, 
+     main =  "Histogram of 1/4 mile per second variable", 
+     xlab = "1/4 mile per second"
+) 
+summary(data$qsec)
+
+
+# ploting variables against each other 
+ ?plot
+plot(
+  x = data$disp, 
+  y = data$mpg, 
+  main = "Miles per gallon against displacement", 
+  xlab = "displacement", 
+  ylab = "mpg"
+)
+
+
+# created a faceted plot , add the third dimension to an x-y scatter plot 
+?coplot
+coplot( 
+  formula = disp ~ mpg | as.factor(cyl), 
+  data = data, 
+  panel = panel.smooth, 
+  rows =1
+  )
+
+
+# pairs -- create scatter plots for pair-wise combinations 
+? pairs
+col_pairs <- c("mpg", "cyl", "disp", "qsec")
+pairs(data[,col_pairs], main = "mtcars data")
