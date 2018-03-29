c0872da5c0ca072f60cd57eb28c6a460b6542937
mutaz mdsi <mutaz.m.abughazaleh@student.uts.edu.au>
2018-03-30 02:38:16 +1100

    start summarising sleep events into a new df

--- misc/rjson/rjson.r
+++ misc/rjson/rjson.r
@@ -15,8 +15,17 @@ for (sleep_record in sleep_data)
   print(paste("Stop:", sleep_record$stop))
   print(paste("number of events:", length(sleep_record$events)))
   se <- matrix(unlist(sleep_record$events), ncol = 3, byrow = TRUE)
-
+  sleep_record$num_sleepevents <- length(sleep_record$events)
+  # sleep_record$events <- NULL
 }
 
 print("first few records from sleep events list")
 head(se)
+
+
+# summarising sleepevents into a new df
+lapply(X = sleep_data, FUN = function(x) { 
+  return (  data.frame( start = x$start, 
+                        stop = x$stop, 
+                        num_sleepevents = length(x$events)))
+            })
