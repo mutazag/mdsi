0e10005c4c651d4557d49f9179893dfb34ac4928
mutaz mdsi <mutaz.m.abughazaleh@student.uts.edu.au>
2018-04-15 22:51:01 +1000

    DAM textmining - load corpus remove punctuations and numbers remove stop words stemming NEXT: create document term matrix (DTM) and start exploring

--- DAM/textmining/intro.R
+++ DAM/textmining/intro.R
@@ -3,3 +3,38 @@
 # set working directory
 setwd("c:/mdsi/dam/textmining")
 
+# load text mining library
+library(tm)
+
+# load corpus
+docs <- Corpus(DirSource("text"))
+
+# list of files in corpus 
+summary(docs)
+
+# inspect a single document
+inspect(docs[[1]])
+writeLines(as.character(docs[[1]]))
+
+
+# tm transformation functions 
+getTransformations()
+
+# define toSpace function 
+toSpace <- content_transformer(
+  function(x, pattern){
+    return (gsub(pattern, " ", x))
+  }
+)
+
+# use tm_map to replace - and :
+docs <- tm_map(docs, toSpace, "-")
+docs <- tm_map(docs, toSpace, ":")
+
+# replace punctuations with space 
+docs <- tm_map(docs, removePunctuation)
+
+# remove non standard punctuations found in text 
+docs <- tm_map(docs, toSpace, "'")
+docs <- tm_map(docs, toSpace, "`")  
+docs <- tm_map(docs, toSpace, " -")
