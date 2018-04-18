85485b0a1ad90dd3a01a2fde467dbc76f9837877
mutaz mdsi <mutaz.m.abughazaleh@student.uts.edu.au>
2018-04-18 20:27:58 +1000

    DAM AT1 - finshed data cleansing

--- DAM/at1/01-cleaning.R
+++ DAM/at1/01-cleaning.R
@@ -22,18 +22,11 @@ num_docs <- length(filenames)
 
 
 ## prepare a content transformation function 
-## that replaces a pattern with space 
+## to replace a pattern with space 
 ## this function will be useful to cleanup 
-## odd characters that are not picked up 
+## odd characters that are not picked up by
 ## tm standard transformations (getTransformations)
 
-
-# define corpus export function 
-saveCorpToDisk <- function(corpus, folder, filenames){
-  tmp_folder <- folder
-  if (!file.exists(tmp_folder)) { dir.create(file.path(".", tmp_folder))}
-  writeCorpus(corpus, path = tmp_folder, filenames = filenames)
-}
 
 # define toSpace function 
 toSpace <- content_transformer(
@@ -43,33 +36,47 @@ toSpace <- content_transformer(
 )
 
 
+# remove chars by utf code 
+
+removeUtfChars <- content_transformer(
+  function(x, utfList = c("\x96", "\x86", "\x86", "\x91")){
+    utfList <- utf8::as_utf8(utfList)
+    utfPattern <- paste0("[",paste(utfList, collapse = "|"),"]")
+    return(gsub(utfPattern, " ", x))
+  }
+)
+
+# define corpus export function to save and check 
+# corpus after transformation/cleanup steps 
+saveCorpToDisk <- function(corpus, folder, filenames){
+  tmp_folder <- folder
+  if (!file.exists(tmp_folder)) { dir.create(file.path(".", tmp_folder))}
+  writeCorpus(corpus, path = tmp_folder, filenames = filenames)
+}
+
+
 ## apply data cleansing 
 # Remove punctuation 
 #Transform to lower case
 #Strip digits
 #Remove stopwords from standard stopword list 
 #Strip whitespace (cosmetic?)
-
-# corpus <- tm_map(corpus, removePunctuation)
+saveCorpToDisk(corpus, "temp4", filenames)
+corpus <- tm_map(corpus, removePunctuation)
+saveCorpToDisk(corpus, "temp4", filenames)
 corpus <- tm_map(corpus, content_transformer(tolower))
-# corpus <- tm_map(corpus, removeNumbers)
+saveCorpToDisk(corpus, "temp4", filenames)
+corpus <- tm_map(corpus, removeNumbers)
+saveCorpToDisk(corpus, "temp4", filenames)
 corpus <- tm_map(corpus, removeWords, stopwords("en"))
-corpus <- tm_map(corpus, stripWhitespace)
+saveCorpToDisk(corpus, "temp4", filenames)
 
-cc <- "[\x96|\x85]"
-# corpus <- tm_map(corpus, removeWords, c("\x96", "\x85"))
 
-utflist <- c(utf8::as_utf8("\x96"),
-             utf8::as_utf8("\x85"),
-             utf8::as_utf8("\x86")) 
-ppUtf <- paste0("[",paste(utflist, collapse = "|"),"]")
-
-# save before 
-saveCorpToDisk(corpus, "temp2", filenames)
 # corpus <- tm_map(corpus, content_transformer(gsub), pattern = ppUtf, replacement = " ") # works
-corpus <- tm_map(corpus, toSpace, ppUtf)
+# corpus <- tm_map(corpus, toSpace, ppUtf)
 #save after 
-saveCorpToDisk(corpus, "temp2", filenames)
+corpus <- tm_map(corpus, removeUtfChars)
+saveCorpToDisk(corpus, "temp4", filenames)
 
 
 corpus <- tm_map(corpus, content_transformer(enc2utf8))
@@ -77,8 +84,16 @@ corpus <- tm_map(corpus, removeWords, c(utf8::as_utf8("\x96"),
                                         utf8::as_utf8("\x85"),
                                         utf8::as_utf8("\x86")) )
 
-saveCorpToDisk(corpus, "temp2", filenames)
+saveCorpToDisk(corpus, "temp4", filenames)
+corpus <- tm_map(corpus, stripWhitespace)
 
+
+### note: some tansformers need to be run more than once 
+### e.g tolower did not transform entire doc to lower the first time 
+
+corpus <- tm_map(corpus, toSpace, utf8::as_utf8("\x85"))
+# save before 
+saveCorpToDisk(corpus, "temp4", filenames)
 ## replacing other encoded char
 # stringi::stri_replace_all_regex(corpus[[34]], "[^\x20-\x7E]", " ")
 # corpus <- tm_map(corpus, toSpace, "[^\x20-\x7E]")
@@ -91,12 +106,4 @@ saveCorpToDisk(corpus, "temp2", filenames)
 # corpus <- tm_map(corpus, toSpace, "[^a-z,A-Z, ]")  
 
 
-
-# export corpus to disk and inspect using bash and regex
-# for any non-word characters that still exist
-tmp_folder <- "tmp"
-if (!file.exists(tmp_folder)) { dir.create(file.path(".", tmp_folder))}
-writeCorpus(corpus, path = tmp_folder, filenames = filenames)
-
-#Stem document
 
