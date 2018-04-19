23fea50aea6d099e64e681d94a52d0eb43ecc12f
mutaz mdsi <mutaz.m.abughazaleh@student.uts.edu.au>
2018-04-19 12:18:57 +1000

    DAM AT1 - topic modeling - output for multiple k intial inspection of topic/terms to extract meaning

--- DAM/at1/04-topicmodel.R
+++ DAM/at1/04-topicmodel.R
@@ -16,10 +16,10 @@ rm(list = ls())  # cleanup env
 setwd("c:/mdsi/dam/at1")
 docs_folder <- "stem"
 docs <- Corpus(DirSource(docs_folder, encoding = "UTF-8"))
-filenames <- meta(docs,"id")
-num_docs <- length(filenames)
 
-dtm <- DocumentTermMatrix(docs, 
+num_docs <- length(docs)
+
+mydtm <- DocumentTermMatrix(docs, 
                           control=list(
                             wordLengths=c(4, 20),
                             bounds = list(global = c(3,29))
@@ -27,8 +27,8 @@ dtm <- DocumentTermMatrix(docs,
 )
 
 # use filenames as a row name in dtm
-rownames(dtm) <- filenames
-termFreq <- colSums(as.matrix(dtm))
+rownames(mydtm) <- meta(docs,"id")
+termFreq <- colSums(as.matrix(mydtm))
 termFreq <- data.frame(term=names(termFreq), count=termFreq)
 ord <- order(termFreq$count, decreasing = T)
 termFreq <- termFreq[ord,]
@@ -64,48 +64,88 @@ ctrl <-  list(nstart = nstart,
 # number of topics k
 k <- 5
 
-ldaout <- LDA(dtm, 
-              k, 
-              method = "Gibbs",
-              control = ctrl
-              )
 
-
-# get the document to topic mapping
-ldaout.topics <- as.matrix(topics(ldaout))
-ldaout.topics <- data.frame(filename = rownames(ldaout.topics), topic = ldaout.topics)
-
-# top 6 terms in each topic
-ldaout.terms <- as.matrix(terms(ldaout,6))
-
-# topic assignment probablities 
-ldaout.topicProbabilities <- as.data.frame(ldaout@gamma)
-rownames(ldaout.topicProbabilities) <- filenames
-
-
-
-# Calculating relative importance of topics in a document
-# sort topic probabilities and then compare p(k) element (highest)
-# and p(k-1) element (second highest), and then second highest
-# with the third highest p(k-2)
-# p(k)/p(k-1) and p(k-1)/p(k-2)
-
-sort(ldaout.topicProbabilities[1,])[k]
-sort(ldaout.topicProbabilities[1,])[k-1]
-sort(ldaout.topicProbabilities[1,])[k-2]
-
-topic1ToTopic2 <- lapply(1:nrow(ldaout.topicProbabilities), function(i, tp, k){
-      return (sort(tp[i,])[k]/sort(tp[i,])[k-1])
-    }, 
-    ldaout.topicProbabilities,
-    k)
-topic2ToTopic3 <- lapply(1:nrow(ldaout.topicProbabilities), function(i, tp, k){
+topicModelingLDA <- function(dtm, k, ctrl){
+  
+  filenames <- rownames(dtm)
+  ldaout <- LDA(dtm, 
+                k, 
+                method = "Gibbs",
+                control = ctrl
+  )
+  
+  # get the document to topic mapping
+  ldaout.topics <- as.matrix(topics(ldaout))
+  ldaout.topics <- data.frame(filename = filenames, topic = ldaout.topics)
+  
+  # top 6 terms in each topic
+  ldaout.terms <- as.matrix(terms(ldaout,6))
+  ldaout.terms <- data.frame(ldaout.terms)
+  
+  # topic assignment probablities 
+  ldaout.topicProbabilities <- as.data.frame(ldaout@gamma)
+  ldaout.topicProbabilities <- data.frame(filename = filenames, ldaout.topicProbabilities )
+  rownames(ldaout.topicProbabilities) <- filenames
+  
+  
+  # Calculating relative importance of topics in a document
+  # sort topic probabilities and then compare p(k) element (highest)
+  # and p(k-1) element (second highest), and then second highest
+  # with the third highest p(k-2)
+  # p(k)/p(k-1) and p(k-1)/p(k-2)
+  
+  
+  topic1ToTopic2 <- lapply(1:nrow(ldaout.topicProbabilities), function(i, tp, k){
+    return (sort(tp[i,])[k]/sort(tp[i,])[k-1])
+  }, 
+  ldaout.topicProbabilities,
+  k)
+  topic2ToTopic3 <- lapply(1:nrow(ldaout.topicProbabilities), function(i, tp, k){
     return (sort(tp[i,])[k-1]/sort(tp[i,])[k-2])
-    }, 
-    ldaout.topicProbabilities,
-    k)
+  }, 
+  ldaout.topicProbabilities,
+  k)
+  
+  topic2topic <- data.frame(filename = filenames, 
+                            topic1ToTopic2 = unlist(topic1ToTopic2), 
+                            topic2ToTopic3 = unlist(topic2ToTopic3))
+  
+  
+  
+  ## save outputs to files
+  
+  write.csv(ldaout.topics, file=paste0("lda-",k,"-DocsToTopics.csv"), row.names = F)
+  write.csv(ldaout.terms, file=paste0("lda-",k,"-TopicsToTerms.csv"))
+  write.csv(ldaout.topicProbabilities, file=paste0("lda-",k,"-TopicProbablities.csv"), row.names = F)
+  write.csv(topic2topic, file=paste0("lda-",k,"-Topic1to2_2to3.csv"))
+}
 
-topic2topic <- data.frame(filename = filenames, 
-                          topic1ToTopic2 = unlist(topic1ToTopic2), 
-                          topic2ToTopic3 = unlist(topic2ToTopic3))
+
+## call the topic modeling function and examine outputs 
+## run the topic modeling for fours values of k <- 4:8
+lapply(4:8, function(x) { topicModelingLDA(dtm=mydtm, k=x, ctrl=ctrl)})
+# topicModelingLDA(dtm = mydtm, k = k, ctrl = ctrl)
+
+
+# some intuitive meaning is found when k = 6
+topic2term_6 <- read.csv("lda-6-topicstoterms.csv")
+topic2term_6
+
+
+# X  Topic.1 Topic.2 Topic.3 Topic.4  Topic.5  Topic.6
+# 1 1 document practic organis project knowledg    model
+# 2 2     word    best    work    risk     issu     data
+# 3 3  cluster problem   manag   manag question techniqu
+# 4 4    topic   paper   chang process     idea     mani
+# 5 5     term  reason   organ  social    figur     base
+# 6 6   corpus possibl  system  involv     said function
+
+# based on topic/term matrix, topics labels could be: 
+# topic 1: term clustering 
+# topic 2: best practice discussion
+# topic 3: orgnisation change magement
+# topic 4: project/risk management
+# topic 5: knowledge and issues! 
+# topic 6: data modeling techniques
+
 
