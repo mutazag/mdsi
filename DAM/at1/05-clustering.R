e370ddacea30c8cb8a6169577b9e25f0c5b632ff
mutaz mdsi <mutaz.m.abughazaleh@student.uts.edu.au>
2018-04-19 14:58:27 +1000

    DAM AT1 - clustering - start

---
+++ DAM/at1/05-clustering.R
@@ -0,0 +1,13 @@
+## 36106 - Data Algorithms and Meaning
+## Assignment 1: Analysis and interpertation of unstructured data
+##
+## Mutaz Abu Ghazaleh 
+## 13184383
+##
+## Clustering
+
+## Library
+
+library(tm)
+rm(list = ls())  # cleanup env 
+setwd("c:/mdsi/dam/at1")  # set working directory
