625b610eb6a11ff48618ce32e742bc1d5d1ab6e3
mutaz mdsi (on book) <mutaz.m.abughazaleh@student.uts.edu.au>
2018-05-09 23:18:44 +1000

    addded zscore and centering scaling methods, plotting correlation

--- misc/minmax_scaling.R
+++ misc/minmax_scaling.R
@@ -10,3 +10,32 @@ par(mfrow=c(1,2))
 hist(x,          breaks=10, xlab="Data",            col="lightblue", main="")
 hist(normalized, breaks=10, xlab="Normalized Data", col="lightblue", main="")
 plot(x, normalized)
+
+
+# center x but no scaling 
+centered <- scale(x, center = TRUE, scale = FALSE)
+zscore <- scale(x, center = TRUE, scale = TRUE)
+
+par(mfrow=c(2,2))
+hist(x,          breaks=10, xlab="Data",            col="lightblue", main="")
+hist(normalized, breaks=10, xlab="Normalized Data", col="lightblue", main="")
+hist(centered,          breaks=10, xlab="centered",            col="lightblue", main="")
+hist(zscore, breaks=10, xlab="z-score", col="lightblue", main="")
+
+par(mfrow=c(1,1))
+df <- data.frame(x, normalized, centered, zscore)
+plot(df)
+
+
+#plot correlations
+#http://www.sthda.com/english/wiki/visualize-correlation-matrix-using-correlogram
+library(corrplot)
+xcor <- cor(mtcars)
+corrplot(xcor, method = "circle")
+corrplot(xcor, method = "pie")
+corrplot(xcor, method = "color")
+corrplot(xcor, method = "number")
+corrplot(xcor, method = "number", type="upper")
+corrplot(xcor, method = "number", type="lower", order="hclust")
+corrplot(xcor, method = "circle", type="lower", order="hclust", col=c("black", "white"), bg="lightblue")
+corrplot(xcor, method = "circle", type="lower", order="hclust", addCoef.col = "black")
