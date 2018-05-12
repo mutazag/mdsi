0a7785cf8a1793d9ebc3684ec0dbe9bb4a6a863a
mutaz mdsi (on book) <mutaz.m.abughazaleh@student.uts.edu.au>
2018-05-12 21:26:00 +1000

    DAM at2a: plotting industry and location selerately, plotting mothly for 1 industry/location, plotting the peaks and troughs on a month by month with one line for each year, created a basic LM model with all the variabls. now i want to create lagged features using rolling sum/mean

--- DAM/at2a/assignment-instructions.R
+++ DAM/at2a/assignment-instructions.R
@@ -55,6 +55,53 @@ plot_industry <- function (df)
     )
 }
 
+plot_monthly <- function(df_agg, i=1, l=1, showlm = FALSE)
+{
+  print("industry ~ location plot")
+  df_filter <- df_agg %>% filter(industry %in% i, location %in% l)
+  
+  p <- df_filter %>% 
+    ggplot(aes(x=date, y=monthly_mean)) + 
+    geom_line() + 
+    facet_grid(industry ~ location,  labeller = label_both ) +
+    scale_y_continuous(labels = scales::comma) +
+    labs(title = "Mean Monthly Sales over Date", 
+         caption = paste0("Industry: ", i, ". Location: ", l),
+         x = "Date", 
+         y = "Mean Monthly Sales ($)")
+  
+  if (showlm == TRUE) {
+    p <- p + geom_smooth(method = "lm", se = FALSE)
+  }
+  
+  p
+}
+
+plot_ym <- function(df_agg_ym, i=1, l=1, showlm = FALSE)
+{
+  print("monthly on year by year")
+  df_filter <- df_agg_ym %>% filter(industry %in% i, location %in% l)
+  
+  p <- df_filter %>% 
+    ggplot(aes(x=m, y=monthly_mean, color=factor(y))) + 
+    geom_line()  +
+    scale_x_continuous(breaks = 1:12, labels = month.name, minor_breaks = NULL) +
+    scale_y_continuous(labels = scales::comma) +
+    theme(axis.text.x = element_text(angle=90)) + 
+    labs(title = "Mean Monthly Sales over Date", 
+         subtitle = "Month View by Year", 
+         caption = paste0("Industry: ", i, ". Location: ", l), 
+         x = "Date", 
+         y = "Mean Monthly Sales ($)", 
+         colour = "Year")
+  
+  if (showlm == TRUE) {
+    p <- p + geom_smooth(method = "lm", se = FALSE)
+  }
+  
+  p
+}
+
 #### Task 1 ####
 
 # Create	an	aggregated	data	set	of	the	fields	date, industry
@@ -62,7 +109,8 @@ plot_industry <- function (df)
 
 df_agg <- df %>% 
   group_by(date,industry, location) %>%
-  summarise(monthly_mean = mean(monthly_amount))
+  summarise(monthly_mean = mean(monthly_amount)) %>% 
+  ungroup()
 
 df_agg %>% plot_industry()
 df_agg %>% plot_location()
@@ -75,12 +123,19 @@ df_agg %>% plot_location()
 
 df_agg1 <- df_agg %>% filter(industry == 1, location == 1)
 
-df_agg1 %>% 
-  ggplot(aes(x=date, y=monthly_mean)) + 
-  geom_line() + facet_grid(industry ~ location )
+df_agg %>% plot_monthly(1,1,showlm = T)
+# we can notice peaks and troughts in this chart for industry = 1/location = 1.
+# Can we observe a discernible patter on a month by month basis? for thati will
+# plot the same filtered data set but facet by year however each peak seems to
+# be slighty higher than previous ones, and similarly each trough also follows a
+# similar positive trend, which implies an global positive trend through out the
+# years
 
 
+df_agg_ym <- df_agg %>% 
+  mutate(m = month(date), y = year(date))
 
+df_agg_ym %>% plot_ym(1,1,T)
 #### Task 3 - lm #### 
 
 # For	industry =	1	and	location	=	1,	train a	linear	regression	model
