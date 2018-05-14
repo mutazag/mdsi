042bb77e645d144b6e0a7b0c73d12abdb55206d6
mutaz mdsi (on book) <mutaz.m.abughazaleh@student.uts.edu.au>
2018-05-15 02:21:17 +1000

    how to calculate lagged values

---
+++ misc/lagged_values.R
@@ -0,0 +1,35 @@
+# https://stackoverflow.com/questions/11389415/calculating-lagged-values
+
+
+#Start with a simple one column matrix
+x <- matrix(1:6, ncol = 1)
+
+#Compute the diffence, pad with an NA at the front, and then divide by x
+cbind(x, c(NA, diff(x))/x)
+
+
+# https://danieljhocking.wordpress.com/2014/12/03/lags-and-moving-means-in-dplyr/
+df = expand.grid(site = factor(seq(2)),
+                 year = 2000:2004,
+                 day = 1:4)
+# use Poisson to make math easy to check moving means of temperature
+df$temp = rpois(dim(df)[1], 5)
+
+
+# moving mean for that day and previous days (e.g. 5 represents the mean of that day and the for previous days)
+df2 = df %>%
+  group_by(site, year) %>%
+  arrange(site, year, day) %>%
+  mutate(temp.5 = rollmean(x = temp, 5, align = "right", fill = NA))
+head(df2, 75)
+
+# moving mean for the previous days not including the current day (e.g. 5 represents the mean of the 5 previous days)
+df3 = df %>% group_by(site) %>% arrange(site, year,day) %>%
+  mutate(temp.lag1 = lag(temp, n = 1)) %>% 
+  mutate(temp.5.previous = rollapply(data = temp.lag1, 
+                                     width = 5, 
+                                     FUN = mean, 
+                                     align = "right", 
+                                     fill = NA, 
+                                     na.rm = T))
+head(df2, 75)
